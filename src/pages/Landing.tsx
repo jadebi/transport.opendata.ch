fd3c18ef95c74abe0521c4e6b259db1bdd3c9129
Jannick Tobler <kleinermatick@gmail.com>
2025-12-05 20:27:12 +0100

    ‼️ BREAKING: fresh start with HeroUI

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,30 +1,35 @@
+import { Button, Card, Divider } from '@heroui/react'
 import { useState } from 'react'
+import { SBB } from "../assets/icons"
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [count, setCount] = useState(0)
+  function setFastCount(props: boolean) {
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          vite
-        </a>
-        <a href="https://react.dev" target="_blank">
-          react
-        </a>
+      <div className='w-full flex flex-col items-center'>
+        <div className='w-min flex flex-col items-center'>
+          <span className='text-5xl mt-2 mx-2'>
+            <b>Stationboard</b>
+          </span>
+          {/* <Divider className='my-1' />
+          <span className='text-black/50 dark:text-white/50'> test </span> */}
+        </div>
+
+        <Divider className='my-2' />
+
+        <div className='w-full flex flex-col items-center'>
+          <Button onPress={() => setCount(count + 1)} onPressStart={() => (setFastCount(true))}>
+            {(count)}
+          </Button>
+          <div className='grid grid-cols-8 grid-flow-row'>
+            {Array.from({ length: count }).map(() => (<Card className='border m-1 max-h-28 max-w-45'><SBB className='h-45 w-45 block' /></Card>))}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
